24f74c23b212463ddceda94b4f369f39bcdc14bc
chaki@sei.cmu.edu <chaki@sei.cmu.edu@780269f3-a799-4dce-8944-02d22917db73>
2008-09-26 19:32:59 +0000

    added functions to create and destroy integer, rational, and double constants. added a function to create and return a DDD theory.
    git-svn-id: https://svn.pls.sei.cmu.edu/pacc/projects/misc/ddd@1538 780269f3-a799-4dce-8944-02d22917db73

--- src/tdd/tdd-ddd/tdd-ddd.c
+++ src/tdd/tdd-ddd/tdd-ddd.c
@@ -3,3 +3,78 @@
  *********************************************************************/
 
 #include "tdd-ddd.h"
+
+/**********************************************************************
+ * create an integer constant
+ *********************************************************************/
+constant_t ddd_create_int_cst(int v)
+{
+  int *res = (int*)malloc(sizeof(int));
+  *res = v;
+  return (constant_t)res;
+}
+
+/**********************************************************************
+ * destroy an integer constant
+ *********************************************************************/
+void ddd_destroy_int_cst(constant_t c)
+{
+  free((int*)c);
+}
+
+/**********************************************************************
+ * create a rational constant. we use the div_t datatype to store the
+ * numerator and denominator.
+ *********************************************************************/
+constant_t ddd_create_rat_cst(int n,int d)
+{
+  div_t *res = (div_t*)malloc(sizeof(div_t));
+  res->quot = n;
+  res->rem = d;
+  return (constant_t)res;
+}
+
+/**********************************************************************
+ * destroy a rational constant
+ *********************************************************************/
+void ddd_destroy_rat_cst(constant_t c)
+{
+  free((div_t*)c);
+}
+
+/**********************************************************************
+ * create a double constant
+ *********************************************************************/
+constant_t ddd_create_double_cst(double v)
+{
+  double *res = (double*)malloc(sizeof(double));
+  *res = v;
+  return (constant_t)res;
+}
+
+/**********************************************************************
+ * destroy a double constant
+ *********************************************************************/
+void ddd_destroy_double_cst(constant_t c)
+{
+  free((double*)c);
+}
+
+/**********************************************************************
+ * create a DDD theory
+ *********************************************************************/
+theory_t ddd_create_theory()
+{
+  theory_t res;
+  res.create_int_cst = ddd_create_int_cst;
+  res.destroy_int_cst = ddd_destroy_int_cst;
+  res.create_rat_cst = ddd_create_rat_cst;
+  res.destroy_rat_cst = ddd_destroy_rat_cst;
+  res.create_double_cst = ddd_create_double_cst;
+  res.destroy_double_cst = ddd_destroy_double_cst;
+  return res;
+}
+
+/**********************************************************************
+ * end of tdd-ddd.c
+ *********************************************************************/
