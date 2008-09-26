c7c5d07bbf9615ef681d2603626bf97e02d00aa2
chaki@sei.cmu.edu <chaki@sei.cmu.edu@780269f3-a799-4dce-8944-02d22917db73>
2008-09-26 18:59:02 +0000

    Makefile, and rudimentary source and header files added. the source and header files are basically empty shells right now. have to fill them up with something useful.
    git-svn-id: https://svn.pls.sei.cmu.edu/pacc/projects/misc/ddd@1536 780269f3-a799-4dce-8944-02d22917db73

---
+++ src/tdd/tdd-ddd/tdd-ddd.h
@@ -0,0 +1,12 @@
+/**********************************************************************
+ * This is a private header file to be included from tdd-ddd.c. It is
+ * not meant to be visible externally.
+ *********************************************************************/
+
+#ifndef __TDD_DDD_H__
+#define __TDD_DDD_H__
+
+#include <stdio.h>
+#include "../theory.h"
+
+#endif //__TDD_DDD_H__
