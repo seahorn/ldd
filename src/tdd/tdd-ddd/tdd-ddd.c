c5ca9206f6124525d0f12c01add5f690f874ab5e
chaki@sei.cmu.edu <chaki@sei.cmu.edu@780269f3-a799-4dce-8944-02d22917db73>
2008-09-28 14:39:38 +0000

    added procedure to create DDD terms. i have made a choice about how variables and coefficients are to be input to this procedure. basically, it is provided as an array of alternating coefficients and variables. this can be changed later on.
    git-svn-id: https://svn.pls.sei.cmu.edu/pacc/projects/misc/ddd@1543 780269f3-a799-4dce-8944-02d22917db73

--- src/tdd/tdd-ddd/tdd-ddd.c
+++ src/tdd/tdd-ddd/tdd-ddd.c
@@ -9,7 +9,7 @@
  *********************************************************************/
 constant_t ddd_create_int_cst(int v)
 {
-  ddd_const_t *res = (ddd_const_t*)malloc(sizeof(ddd_const_t));
+  ddd_cst_t *res = (ddd_cst_t*)malloc(sizeof(ddd_cst_t));
   res->type = DDD_INT;
   res->int_val = v;
   return (constant_t)res;
@@ -21,7 +21,7 @@ constant_t ddd_create_int_cst(int v)
  *********************************************************************/
 constant_t ddd_create_rat_cst(int n,int d)
 {
-  ddd_const_t *res = (ddd_const_t*)malloc(sizeof(ddd_const_t));
+  ddd_cst_t *res = (ddd_cst_t*)malloc(sizeof(ddd_cst_t));
   res->type = DDD_RAT;
   res->rat_val.quot = n;
   res->rat_val.rem = d;
@@ -33,7 +33,7 @@ constant_t ddd_create_rat_cst(int n,int d)
  *********************************************************************/
 constant_t ddd_create_double_cst(double v)
 {
-  ddd_const_t *res = (ddd_const_t*)malloc(sizeof(ddd_const_t));
+  ddd_cst_t *res = (ddd_cst_t*)malloc(sizeof(ddd_cst_t));
   res->type = DDD_DBL;
   res->dbl_val = v;
   return (constant_t)res;
@@ -44,7 +44,7 @@ constant_t ddd_create_double_cst(double v)
  *********************************************************************/
 void ddd_destroy_cst(constant_t c)
 {
-  free((ddd_const_t*)c);
+  free((ddd_cst_t*)c);
 }
 
 /**********************************************************************
@@ -52,14 +52,13 @@ void ddd_destroy_cst(constant_t c)
  *********************************************************************/
 bool ddd_is_pinf_cst(constant_t c)
 {
-  ddd_const_t *x = (ddd_const_t*)c;
+  ddd_cst_t *x = (ddd_cst_t*)c;
   switch(x->type)
     {
     case DDD_INT:
       return x->int_val == INT_MAX;
     case DDD_RAT:
-      return (x->rat_val.rem == 0 && x->rat_val.quot > 0) || 
-        (x->rat_val.quot == INT_MAX); 
+      return (x->rat_val.quot == INT_MAX); 
     case DDD_DBL:
       return isinf(x->dbl_val) == 1;
     default:
@@ -72,20 +71,36 @@ bool ddd_is_pinf_cst(constant_t c)
  *********************************************************************/
 bool ddd_is_ninf_cst(constant_t c)
 {
-  ddd_const_t *x = (ddd_const_t*)c;
+  ddd_cst_t *x = (ddd_cst_t*)c;
   switch(x->type)
     {
     case DDD_INT:
       return x->int_val == INT_MIN;
     case DDD_RAT:
-      return (x->rat_val.rem == 0 && x->rat_val.quot < 0) || 
-        (x->rat_val.quot == INT_MIN); 
+      return (x->rat_val.quot == INT_MIN); 
     case DDD_DBL:
       return isinf(x->dbl_val) == -1;
     default:
       return 0;
     }
 }
+
+/**********************************************************************
+ * create a linear term. the first argument is an array of alternating
+ * variables and coefficients. the second argument is the size of the
+ * first argument.
+ *********************************************************************/
+linterm_t ddd_create_linterm(int* coeff_var, size_t n)
+{
+  /* for DDDs, the terms are of the form X-Y. hence n = 4, the first
+     coefficient is +1 and the second coefficient is -1. */
+  assert(n == 4 && coeff_var[0] == 1 && coeff_var[3] == -1);
+  ddd_term_t *res = (ddd_term_t*)malloc(sizeof(ddd_term_t));
+  res->var1 = coeff_var[1];
+  res->var2 = coeff_var[3];
+  return res;
+}
+
 
 /**********************************************************************
  * create a DDD theory
@@ -100,6 +115,7 @@ theory_t ddd_create_theory()
   res.destroy_cst = ddd_destroy_cst;
   res.is_pinf_cst = ddd_is_pinf_cst;
   res.is_ninf_cst = ddd_is_ninf_cst;
+  res.create_linterm = ddd_create_linterm;
   return res;
 }
 
--- src/tdd/tdd-ddd/tdd-ddd.h
+++ src/tdd/tdd-ddd/tdd-ddd.h
@@ -16,12 +16,12 @@
  * a generic structure used to represent integer, rational, and double
  * constants.
  *********************************************************************/
-typedef enum ddd_const_type { DDD_INT, DDD_RAT, DDD_DBL } ddd_const_type_t;
+typedef enum ddd_cst_type { DDD_INT, DDD_RAT, DDD_DBL } ddd_cst_type_t;
 
-typedef struct ddd_const 
+typedef struct ddd_cst 
 {
   /* the type of the constant */
-  ddd_const_type_t type;
+  ddd_cst_type_t type;
 
   /* the value of the constant */
   union 
@@ -30,7 +30,24 @@ typedef struct ddd_const
     div_t rat_val;
     double dbl_val;
   };
-} ddd_const_t;
+} ddd_cst_t;
+
+/**********************************************************************
+ * a DDD term is of the form X-Y and consists of two variables
+ *********************************************************************/
+typedef struct ddd_term { int var1,var2; } ddd_term_t;
+  
+/**********************************************************************
+ * a DDD constraint is of the form T < C or T <= C where T is a term
+ * and C is a constant
+ *********************************************************************/
+typedef struct ddd_cons
+{ 
+  ddd_term_t term; //the term
+  ddd_cst_t cst; //the constant
+  bool strict; //whether the inequality is strict
+} ddd_cons_t;
+  
 
 #endif //__TDD_DDD_H__
 
