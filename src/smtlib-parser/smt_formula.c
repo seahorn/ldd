173e2c3367a6e7167259fb17140850d386185365
chaki@sei.cmu.edu <chaki@sei.cmu.edu@780269f3-a799-4dce-8944-02d22917db73>
2008-11-25 18:31:46 +0000

    added a C file to contain routines for manipulating SMT formulas.
    git-svn-id: https://svn.pls.sei.cmu.edu/pacc/projects/misc/ddd@1814 780269f3-a799-4dce-8944-02d22917db73

---
+++ src/smtlib-parser/smt_formula.c
@@ -0,0 +1,12 @@
+/**
+ * this file contains a set of utility routines used to manipulate
+ * SMT-LIB formulas.
+ */
+
+#include "smt_formula.h"
+
+void destroy_formula(smt_formula_t *f)
+{
+  switch(f->type) {
+  }
+}
