eb1edb2f9cb3ccee9e77b80764a5f2374bd382f8
chaki@sei.cmu.edu <chaki@sei.cmu.edu@780269f3-a799-4dce-8944-02d22917db73>
2008-11-27 20:37:45 +0000

    added a main function.
    git-svn-id: https://svn.pls.sei.cmu.edu/pacc/projects/misc/ddd@1829 780269f3-a799-4dce-8944-02d22917db73

--- src/sagar-bench/smt_qelim_solver.c
+++ src/sagar-bench/smt_qelim_solver.c
@@ -1,8 +1,41 @@
-#include "smt_qelim_solver.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "smt_qelim_solver.h"
+#include "smt_formula.h"
+
+extern int yyparse(void);
+extern smt_formula_t *smtFormula;
+extern FILE *yyin;
+
+/**********************************************************************
+ * the core solver routine
+ *********************************************************************/
 int smt_qelim_solve(char *fileName,tdd_manager *tdd)
 {
+  //parse SMT file. the AST will be stored in smtFormula
+  yyin = fopen(fileName,"r");
+  if(!yyin) {
+    fprintf(stderr,"ERROR: could not open SMT file %s for parsing!\n",fileName);
+    exit(1);
+  }
+  yyparse();
+  fclose(yyin);
+  
+  //solve the formula
+
+  //cleanup
+  smt_destroy_formula(smtFormula);
+
   return -1;
+}
+
+/**********************************************************************
+ * the main routine
+ *********************************************************************/
+int main(int argc,char *argv[])
+{
+  return 0;
 }
 
 /**********************************************************************
