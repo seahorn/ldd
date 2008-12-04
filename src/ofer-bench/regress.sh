96a128d5868ef3bfd2f3788583213b2ea2e89663
arie@sei.cmu.edu <arie@sei.cmu.edu@780269f3-a799-4dce-8944-02d22917db73>
2008-12-04 16:30:43 +0000

    Added a regression script for ofer-bench
    git-svn-id: https://svn.pls.sei.cmu.edu/pacc/projects/misc/ddd@1846 780269f3-a799-4dce-8944-02d22917db73

---
+++ src/ofer-bench/regress.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# limit to 512MB memory
+ulimit -v 512000
+
+# 4s
+time ./jobsolve -e 1 -m 20 -r 0 tests/ta01.txt
+
+time ./jobsolve -e 1 -m 20 -r 0 tests/ta81.txt
+
+time ./jobsolve -e 1 -m 20 -r 0 tests/ta93.txt
+
+
+time ./jobsolve -e 1 -m 10 -r 0 tests/ofer2.txt
+time ./jobsolve -e 2 -m 10 -r 0 tests/ofer2.txt
+time ./jobsolve -e 3 -m 10 -r 0 tests/ofer2.txt
+
+
+time ./jobsolve -e 1 -m 11 -r 1 tests/ofer2.txt
+time ./jobsolve -e 2 -m 11 -r 1 tests/ofer2.txt
+time ./jobsolve -e 3 -m 11 -r 1 tests/ofer2.txt
+
+time ./jobsolve -e 1 -m 116  -r 0 tests/ofer3.txt
+time ./jobsolve -e 2 -m 116  -r 0 tests/ofer3.txt
+time ./jobsolve -e 3 -m 116  -r 0 tests/ofer3.txt
+
+
+time ./jobsolve -e 1 -m 117  -r 1 tests/ofer3.txt
+time ./jobsolve -e 2 -m 117  -r 1 tests/ofer3.txt
+time ./jobsolve -e 3 -m 117  -r 1 tests/ofer3.txt
+
+time ./jobsolve -e 2 -m 443 -r 1 tests/ofer1.txt
+time ./jobsolve -e 2 -m 444 -r 0 tests/ofer1.txt
+
+# 10s 
+time ./jobsolve -e 1 -m 443 -r2 -l3 tests/ofer1.txt
+# -e3 fails completelly
+
+# 10s
+time ./jobsolve -e 1 -m 444 -r2 -l3  tests/ofer1.txt
+
+time ./jobsolve -e 1 -m 250  -r0  tests/ofer1.txt
+
+time ./jobsolve -e 1 -m 282  -r0  tests/ofer1.txt
+
+time ./jobsolve -e 1 -m 283  -r0  tests/ofer1.txt
+
+
+time ./jobsolve -e 2 -m 433 -r0   tests/ta81.txt 
+
+time ./jobsolve -e 2 -m 434 -r1   tests/ta81.txt 
+
+time ./jobsolve -e2 -m 458 -r0  tests/ta82.txt  
+time ./jobsolve -e2 -m 459 -r1  tests/ta82.txt  
+
+# 2m
+#time ./jobsolve -e 1 -m 20 -r 0 tests/ta50.txt
+
+# 1m
+#time ./jobsolve -e 1 -m 290  -r0  tests/ofer1.txt
+# 2m
+#time ./jobsolve -e 1 -m 305  -r0 tests/ofer1.txt
