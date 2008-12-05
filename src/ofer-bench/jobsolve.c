50216c7cd6c68d2d4c3daa81c2475b6b8743eb2f
arie@sei.cmu.edu <arie@sei.cmu.edu@780269f3-a799-4dce-8944-02d22917db73>
2008-12-05 16:05:42 +0000

    Jobsolve with tdd_sat_reduce and new regressions
    git-svn-id: https://svn.pls.sei.cmu.edu/pacc/projects/misc/ddd@1854 780269f3-a799-4dce-8944-02d22917db73

--- src/ofer-bench/jobsolve.c
+++ src/ofer-bench/jobsolve.c
@@ -32,6 +32,9 @@ int verbose = 0;
 static tdd_node *qelim1 ();
 static tdd_node *qelim2 ();
 static tdd_node *qelim3 ();
+static tdd_node *qelim4 ();
+static tdd_node *qelim5 ();
+
 
 int write_tdd (char* filename, DdManager *cudd, tdd_node *f, char** inames)
 {
@@ -75,7 +78,10 @@ void usage ()
 
   printf ("  -e mode\texistential quantification mode\n "
 	  "\t\t 1 tdd_exist_abstract,\n\t\t 2 tdd_exist_abstract_v2\n"
-	  "\t\t 3 tdd_exist_abstract_v2 one var at a time\n");
+	  "\t\t 3 tdd_exist_abstract_v2 one var at a time\n"
+	  "\t\t 4 tdd_exist_abstract with tdd_sat_reduce\n"
+	  "\t\t 5 tdd_exist_abstract_v2 one var at a time "
+	  "with tdd_sat_reduce\n");
 
   printf ("  -m max\tthe maximum for the job schedule\n");
 
@@ -194,6 +200,12 @@ int main (int argc, char** argv)
       break;
     case 3:
       res = qelim3 ();
+      break;
+    case 4:
+      res = qelim4 ();
+      break;
+    case 5:
+      res = qelim5 ();
       break;
     default:
       printf ("Unknown QELIM mode: %d\n", qelim_mode);
@@ -418,6 +430,227 @@ qelim3 ()
 
       /* Only run quantification when needed */
       if (Cudd_IsConstant (res)) break;
+
+      vars [i] = 1;
+      tmp = tdd_exist_abstract_v2 (tdd, res, vars);
+      vars [i] = 0;
+      
+      Cudd_Ref (tmp);
+      Cudd_RecursiveDeref (cudd, res);
+      res = tmp;
+
+      printf ("After quantifying x%d the size is %d\n",
+	      i, Cudd_DagSize (res));
+
+      if (verbose)
+	{
+	  printf ("Sanity check:\n");
+	  tdd_sanity_check (tdd);
+
+
+	  printf ("Writting current result\n");
+	  /* /\*       Cudd_PrintMinterm (cudd, res); *\/ */
+	  snprintf (name, 20, "qelim-%d.dot", i);
+	  write_tdd (name, cudd, res, NULL);
+
+	  tdd_manager_debug_dump (tdd);
+	  t->theory_debug_dump (tdd);
+	
+
+	  printf ("The result of existential quantification is: ");
+	  if (Cudd_ReadOne (cudd) == res)
+	    printf ("true\n");
+	  else if (Cudd_ReadLogicZero (cudd) == res)
+	    printf ("false\n");
+	  else
+	    {
+	      printf ("NON-EMPTY\n");
+	    }
+	}
+    }
+
+  return res;
+ }
+ 
+
+tdd_node *
+qelim4 ()
+{
+  
+  tdd_node *res;
+  unsigned int i;
+  size_t size;
+
+  /* number of variables that were eliminated */
+  size_t count;
+
+  /* elimination order: go from start to end using step */
+  unsigned int start, end, step;
+
+  size = t->num_of_vars (t);
+
+  switch (order)
+    {
+    case 1:
+      /* ascending start from the second variable */
+      step = 1;
+      start = 1;
+      end = size;
+      break;
+    case 2:
+      step = -1;
+      start = size - 1;
+      end = 0;
+      break;
+    case 3:
+      /* ascending starting from the first variable */
+      step = 1;
+      start = 0;
+      end = size;
+      break;
+    default:
+      printf ("Unknown elimination order %d\n", order);
+      abort ();
+    }
+  
+  
+  
+  
+  res = bench;
+
+  for (i = start, count=0; i != end && count < limit; i+=step,count++)
+    {
+      tdd_node* tmp;
+      char name[20];
+      int szBefore, szAfter;
+
+      /* Only run quantification when needed */
+      if (Cudd_IsConstant (res)) break;
+
+
+      szBefore = Cudd_DagSize (res);
+      tmp = tdd_sat_reduce (tdd, res, -1);
+      Cudd_Ref (tmp);
+      Cudd_RecursiveDeref (cudd, res);
+      res = tmp;
+      szAfter = Cudd_DagSize (res);
+      if (szBefore != szAfter)
+	printf ("\tReduced from %d to %d\n", szBefore, szAfter);
+
+
+      tmp = tdd_exist_abstract (tdd, res, i);
+      Cudd_Ref (tmp);
+      Cudd_RecursiveDeref (cudd, res);
+      res = tmp;
+
+      printf ("After quantifying x%d the size is %d\n",
+	      i, Cudd_DagSize (res));
+
+      if (verbose)
+	{
+	  printf ("Sanity check:\n");
+	  tdd_sanity_check (tdd);
+
+
+	  printf ("Writting current result\n");
+	  /* /\*       Cudd_PrintMinterm (cudd, res); *\/ */
+	  snprintf (name, 20, "qelim-%d.dot", i);
+	  write_tdd (name, cudd, res, NULL);
+
+	  tdd_manager_debug_dump (tdd);
+	  t->theory_debug_dump (tdd);
+	
+
+	  printf ("The result of existential quantification is: ");
+	  if (Cudd_ReadOne (cudd) == res)
+	    printf ("true\n");
+	  else if (Cudd_ReadLogicZero (cudd) == res)
+	    printf ("false\n");
+	  else
+	    {
+	      printf ("NON-EMPTY\n");
+/* 	      Cudd_PrintMinterm (cudd, res); */
+	    }
+	}
+    }
+
+  return res;
+}
+
+
+
+tdd_node *
+qelim5 ()
+{
+  tdd_node *res;
+  unsigned int i;
+  size_t size;
+
+  /* variables to be quantified out */
+  bool *vars;
+
+  /* number of variables that were eliminated */
+  size_t count;
+
+  /* elimination order: go from start to end using step */
+  unsigned int start, end, step;
+
+  size = t->num_of_vars (t);
+
+  switch (order)
+    {
+    case 1:
+      /* ascending start from the second variable */
+      step = 1;
+      start = 1;
+      end = size;
+      break;
+    case 2:
+      step = -1;
+      start = size - 1;
+      end = 0;
+      break;
+    case 3:
+      /* ascending starting from the first variable */
+      step = 1;
+      start = 0;
+      end = size;
+      break;
+    default:
+      printf ("Unknown elimination order %d\n", order);
+      abort ();
+    }
+  
+  
+  vars = (bool*) malloc (sizeof(bool) * size);
+  for (i = 0; i < size; i++)
+    vars [i] = 0;
+  
+  
+  res = bench;
+
+  for (i = start, count=0; i != end && count < limit; i+=step,count++)
+    {
+      tdd_node* tmp;
+      char name[20];
+
+
+      int szBefore, szAfter;
+
+      /* Only run quantification when needed */
+      if (Cudd_IsConstant (res)) break;
+
+
+      szBefore = Cudd_DagSize (res);
+      tmp = tdd_sat_reduce (tdd, res, -1);
+      Cudd_Ref (tmp);
+      Cudd_RecursiveDeref (cudd, res);
+      res = tmp;
+      szAfter = Cudd_DagSize (res);
+      if (szBefore != szAfter)
+	printf ("\tReduced from %d to %d\n", szBefore, szAfter);
+
+
 
       vars [i] = 1;
       tmp = tdd_exist_abstract_v2 (tdd, res, vars);
--- src/ofer-bench/regress.sh
+++ src/ofer-bench/regress.sh
@@ -14,23 +14,39 @@ time ./jobsolve -e 1 -m 20 -r 0 tests/ta93.txt
 time ./jobsolve -e 1 -m 10 -r 0 tests/ofer2.txt
 time ./jobsolve -e 2 -m 10 -r 0 tests/ofer2.txt
 time ./jobsolve -e 3 -m 10 -r 0 tests/ofer2.txt
+time ./jobsolve -e 4 -m 10 -r 0 tests/ofer2.txt
+time ./jobsolve -e 5 -m 10 -r 0 tests/ofer2.txt
+
 
 
 time ./jobsolve -e 1 -m 11 -r 1 tests/ofer2.txt
 time ./jobsolve -e 2 -m 11 -r 1 tests/ofer2.txt
 time ./jobsolve -e 3 -m 11 -r 1 tests/ofer2.txt
+time ./jobsolve -e 4 -m 11 -r 1 tests/ofer2.txt
+time ./jobsolve -e 5 -m 11 -r 1 tests/ofer2.txt
+
 
 time ./jobsolve -e 1 -m 116  -r 0 tests/ofer3.txt
 time ./jobsolve -e 2 -m 116  -r 0 tests/ofer3.txt
 time ./jobsolve -e 3 -m 116  -r 0 tests/ofer3.txt
+time ./jobsolve -e 4 -m 116  -r 0 tests/ofer3.txt
+time ./jobsolve -e 5 -m 116  -r 0 tests/ofer3.txt
 
 
 time ./jobsolve -e 1 -m 117  -r 1 tests/ofer3.txt
 time ./jobsolve -e 2 -m 117  -r 1 tests/ofer3.txt
 time ./jobsolve -e 3 -m 117  -r 1 tests/ofer3.txt
+time ./jobsolve -e 4 -m 117  -r 1 tests/ofer3.txt
+time ./jobsolve -e 5 -m 117  -r 1 tests/ofer3.txt
 
-time ./jobsolve -e 2 -m 443 -r 1 tests/ofer1.txt
-time ./jobsolve -e 2 -m 444 -r 0 tests/ofer1.txt
+
+time ./jobsolve -e 2 -m 443 -r 0 tests/ofer1.txt
+time ./jobsolve -e 2 -m 444 -r 1 tests/ofer1.txt
+time ./jobsolve -e 4 -m 443 -r 0 tests/ofer1.txt
+time ./jobsolve -e 4 -m 444 -r 1 tests/ofer1.txt
+time ./jobsolve -e 5 -m 443 -r 0 tests/ofer1.txt
+time ./jobsolve -e 5 -m 444 -r 1 tests/ofer1.txt
+
 
 # 10s 
 time ./jobsolve -e 1 -m 443 -r2 -l3 tests/ofer1.txt
@@ -47,11 +63,20 @@ time ./jobsolve -e 1 -m 283  -r0  tests/ofer1.txt
 
 
 time ./jobsolve -e 2 -m 433 -r0   tests/ta81.txt 
-
 time ./jobsolve -e 2 -m 434 -r1   tests/ta81.txt 
+time ./jobsolve -e 4 -m 433 -r0   tests/ta81.txt 
+time ./jobsolve -e 4 -m 434 -r1   tests/ta81.txt 
+time ./jobsolve -e 5 -m 433 -r0   tests/ta81.txt 
+time ./jobsolve -e 5 -m 434 -r1   tests/ta81.txt 
+
 
 time ./jobsolve -e2 -m 458 -r0  tests/ta82.txt  
 time ./jobsolve -e2 -m 459 -r1  tests/ta82.txt  
+time ./jobsolve -e4 -m 458 -r0  tests/ta82.txt  
+time ./jobsolve -e4 -m 459 -r1  tests/ta82.txt  
+time ./jobsolve -e5 -m 458 -r0  tests/ta82.txt  
+time ./jobsolve -e5 -m 459 -r1  tests/ta82.txt  
+
 
 # 2m
 #time ./jobsolve -e 1 -m 20 -r 0 tests/ta50.txt
