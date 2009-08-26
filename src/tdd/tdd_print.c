5abd5e51f21eb03d32c14057f8a9e0190dcdd0b0
arie@sei.cmu.edu <arie@sei.cmu.edu@780269f3-a799-4dce-8944-02d22917db73>
2009-08-26 14:34:40 +0000

    Updated tdd_print_minterm to remove redundant conjuncts in a minterm
    git-svn-id: https://svn.pls.sei.cmu.edu/pacc/projects/misc/ddd@2928 780269f3-a799-4dce-8944-02d22917db73

--- src/tdd/tdd_print.c
+++ src/tdd/tdd_print.c
@@ -37,6 +37,11 @@ tdd_print_minterm_aux (tdd_manager *tdd, tdd_node *n, int* list)
   DdNode *N, *Nv, *Nnv;
   int i, v, index, p;
   
+  /**
+   * the latest negative constraint to be printed. 
+   * is NULL if there isn't one 
+   */
+  lincons_t negc = NULL;
   
   N = Cudd_Regular (n);
   
@@ -45,31 +50,67 @@ tdd_print_minterm_aux (tdd_manager *tdd, tdd_node *n, int* list)
       /* n == N here implies that n is one */
       if (n == N) 
 	{
+	  /* for each level */
 	  for (i = 0; i < CUDD->size; i++)
 	    {
+	      /* let p be the index of level i */
 	      p = CUDD->invperm [i];
+	      /* let v be the value of p */
 	      v = list [p];
-	      if (v != 2 && tdd->ddVars [p] == NULL) 
-		fprintf (stderr, "%sb%d", (v == 0 ? "!" : " "), p);
-	      else if (v == 0) 
+
+	      if (v == 0 && tdd->ddVars [p] != NULL)
 		{
 		  lincons_t c;
 		  c = THEORY->negate_cons (tdd->ddVars [p]);
-		  THEORY->print_lincons (CUDD->out, c);
-		  THEORY->destroy_lincons (c);
-		  fprintf (CUDD->out, " ");
 		  
-		  /* fprintf (CUDD->out, "!("); */
-		  /* THEORY->print_lincons (CUDD->out,  */
-		  /* 			 tdd->ddVars [p]); */
-		  /* fprintf (CUDD->out, ") "); */
+		  if (negc != NULL)
+		    {
+		      /* print negative constraint if it is not 
+			 implied by c 
+		      */
+		      if (!THEORY->is_stronger_cons (c, negc))
+			{
+			  THEORY->print_lincons (CUDD->out, negc);
+			  fprintf (CUDD->out, " ");
+			}
+		      THEORY->destroy_lincons (negc);
+		    }
+		  
+		  /* store the current constraint to be printed later */
+		  negc = c;
+		  continue;
 		}
+	      
+	      /* if there is a negative constraint waiting to be printed,
+		 print it now 
+	      */
+	      if (negc != NULL)
+		{
+		  THEORY->print_lincons (CUDD->out, negc);
+		  fprintf (CUDD->out, " ");
+		  THEORY->destroy_lincons (negc);
+		  negc = NULL;
+		}
+
+	      /* if v is not a don't care but p does not correspond to 
+	       * a constraint, print it as a Boolean variable */
+	      if (v != 2 && tdd->ddVars [p] == NULL) 
+		fprintf (stderr, "%sb%d", (v == 0 ? "!" : " "), p);
+	      /* v is true */
 	      else if (v == 1)
 		{
 		  THEORY->print_lincons (CUDD->out, 
 					 tdd->ddVars [p]);
 		  fprintf (CUDD->out, " ");
 		}
+	    }
+	  
+	  /* if there is a constraint waiting to be printed, do it now */
+	  if (negc != NULL)
+	    {
+	      THEORY->print_lincons (CUDD->out, negc);
+	      THEORY->destroy_lincons (negc);
+	      negc = NULL;
 	    }
 	  fprintf (CUDD->out, "\n");
 	}
