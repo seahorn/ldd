7ef1935d8bb5d4b3ad7dbfe94944b9819747d9c6
arie@sei.cmu.edu <arie@sei.cmu.edu@780269f3-a799-4dce-8944-02d22917db73>
2011-06-24 18:48:15 +0000

    script to convert PA ldd solver benchmarks into z3 benchmarks
    git-svn-id: https://svn.pls.sei.cmu.edu/pacc/projects/misc/ddd@5521 780269f3-a799-4dce-8944-02d22917db73

---
+++ contrib/ldd2z3.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+###
+### Converts SMT benchmarks for LDDSolver into benchmarks for Z3
+###
+
+import re
+import sys
+import os
+
+
+def main ():
+  predRe = re.compile (r"^:extrapreds \(\((P[0-9]+)\)\)")
+  #varRe = re.compile (r"^[ ]+\((v[0-9])+ FOO\)")
+  varRe = re.compile (r"^  \((v[0-9][^ ]*) FOO")
+  boolVarRe = re.compile (r"^  \((Bbb[0-9][^ ]*) FOO")
+
+  print '(set-option set-param ELIM_QUANTIFIERS true)'
+  sys.stdout.flush ()
+
+  # skip first 5 lines
+  sys.stdin.readline ()
+  sys.stdin.readline ()
+  sys.stdin.readline ()
+  sys.stdin.readline ()
+  sys.stdin.readline ()
+
+  preds = []
+  for line in iter (sys.stdin.readline, ""):
+    m = predRe.match (line)
+    if m:
+      preds.append (m.group (1))
+      continue
+
+    if line.startswith (":formula"):
+      print '(declare-preds ('
+      for p in preds:
+        print '(', p, ')'
+      print '))'
+      print '(simplify'
+      continue
+    
+    m = varRe.match (line)
+    if m:
+      print '  (', m.group(1), 'Int)'
+      continue
+    
+    m = boolVarRe.match (line)
+    if m:
+      print '  (', m.group(1), 'Bool)'
+      continue
+
+
+    sys.stdout.write (line)
+
+  sys.stdout.flush ()
+
+main ()
